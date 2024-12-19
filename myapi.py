8ce4f69f3568e12602940894d09ccededfd3181b
alivkas <mr1504dj@mail.ru>
2024-12-19 17:24:56 +0500

    add web socket

--- myapi.py
+++ myapi.py
@@ -1,16 +1,14 @@
 import asyncio
 
 from fastapi import FastAPI, BackgroundTasks, Depends, HTTPException
-from pydantic import BaseModel
-
+from typing import Dict
 from parser import get_prices
 from starlette.concurrency import run_in_threadpool
 from sqlmodel import Field, SQLModel, create_engine, Session, select
 from sqlalchemy.orm import Session
-
+from starlette.websockets import WebSocket
 
 app = FastAPI()
-PRICES_DB = []
 
 
 class Prices(SQLModel, table=True):
@@ -87,11 +85,12 @@ async def startup_event():
 @app.get("/start_parser")
 async def start_parser(background_tasks: BackgroundTasks):
     background_tasks.add_task(background_add_item)
-    return "Данные обновлены"
+    await broadcast("start_parser", {"message":"start parse"})
 
 
 @app.get("/prices")
 async def read_prices(session: Session = SessionDep, offset: int = 0, limit: int = 1000):
+    await broadcast("read_items", {"message":"prices read"})
     return session.execute(select(Prices).offset(offset).limit(limit)).scalars().all()
 
 
@@ -100,6 +99,8 @@ async def read_item(price_id: int, session: Session = SessionDep):
     price = session.get(Prices, price_id)
     if not price:
         raise HTTPException(status_code=404, detail="Price not found")
+    await broadcast("read_item", {"id":price.id, "name": price.name,
+                                    "price":price.cost})
     return price
 
 
@@ -113,6 +114,8 @@ async def update_item(item_id: int, data: Prices, session: Session = SessionDep)
     session.add(price_db)
     session.commit()
     session.refresh(price_db)
+    await broadcast("update_item", {"id":item_id, "name": price_db.name,
+                                    "price":price_db.cost})
     return price_db
 
 
@@ -123,4 +126,30 @@ async def delete_item(item_id: int, session: Session = SessionDep):
         raise HTTPException(status_code=404, detail="Price not found")
     session.delete(price)
     session.commit()
+    await broadcast("delete_item", {"id":item_id, "name": price.name,
+                                    "price":price.cost})
     return {"ok": True}
+
+
+clients: Dict[str, WebSocket] = {}
+
+
+async def broadcast(event:str, data:dict):
+    for client_id, client in clients.items():
+       try:
+           await client.send_json({"event":event, "data":data})
+       except Exception as e:
+           print(f"error {e} sending message to client {client_id}")
+           del clients[client_id]
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    client_id = str(id(websocket))
+    clients[client_id] = websocket
+    try:
+        while True:
+            await websocket.receive_text()
+    except Exception:
+        del clients[client_id]
